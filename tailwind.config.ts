84463c2875e928393b520f83fe03b458257d3344
Igor Babiy <yegor.babiy@gmail.com>
2026-01-23 19:12:55 +0000

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const tailwindConfig = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#ff3366"
+      }
+    }
+  },
+  plugins: []
+};
+
+export default tailwindConfig;
