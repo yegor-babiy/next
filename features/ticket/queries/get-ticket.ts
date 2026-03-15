c271d1c234fd89a17af08556907e3548f2cae911
Igor Babiy <yegor.babiy@gmail.com>
2026-03-15 20:25:27 +0000

    use prisma client in queries

--- features/ticket/queries/get-ticket.ts
+++ features/ticket/queries/get-ticket.ts
@@ -1,11 +1,9 @@
-import { initialTickets } from "@/data";
-import { Ticket } from "../types";
+import { prisma } from "@/lib/prisma";
 
-export const getTicket = async (ticketId: string): Promise<Ticket | null> => {
-  await new Promise(resolve => setTimeout(resolve, 2000));
-
-  return await new Promise(resolve => {
-    const ticket = initialTickets.find(ticket => ticket.id === ticketId);
-    resolve(ticket || null);
+export const getTicket = async (id: string) => {
+  return await prisma.ticket.findUnique({
+    where: {
+      id
+    }
   });
 };
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -9,4 +9,4 @@ const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export default prisma;
+export { prisma };
