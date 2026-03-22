e7f8c3d86f9782b8fc5bc392ea0daa02d665bff1
Igor Babiy <yegor.babiy@gmail.com>
2026-03-22 20:26:57 +0000

    add delete server action

--- features/ticket/components/ticket-item.tsx
+++ features/ticket/components/ticket-item.tsx
@@ -1,11 +1,12 @@
 import clsx from "clsx";
-import { LucideSquareArrowOutUpRight } from "lucide-react";
+import { LucideSquareArrowOutUpRight, LucideTrash } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Ticket } from "@/lib/generated/prisma/client";
 import { ticketPath } from "@/paths";
+import { deleteTicket } from "../actions/delete-ticket";
 import { TICKET_ICONS } from "../constants";
-import { Ticket } from "../types";
 
 type TicketItemProps = {
   ticket: Ticket;
@@ -19,6 +20,14 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
         <LucideSquareArrowOutUpRight className="w-4 h-4" />
       </Link>
     </Button>
+  );
+
+  const deleteButton = (
+    <form action={deleteTicket.bind(null, ticket.id)}>
+      <Button variant="outline" size="icon">
+        <LucideTrash className="w-4 h-4" />
+      </Button>
+    </form>
   );
 
   return (
@@ -46,9 +55,9 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
         </CardContent>
       </Card>
 
-      {isDetail ? null : (
-        <div className="flex flex-col gap-y-1">{detailButton}</div>
-      )}
+      <div className="flex flex-col gap-y-1">
+        {isDetail ? deleteButton : detailButton}
+      </div>
     </div>
   );
 };
--- features/ticket/types.ts
+++
@@ -1,8 +0,0 @@
-export type TicketStatus = "OPEN" | "IN_PROGRESS" | "DONE";
-
-export type Ticket = {
-  id: string;
-  title: string;
-  content: string;
-  status: TicketStatus;
-};
