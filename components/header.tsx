9960522cc3d1d97e2880fcf8ec30a82b84da80c7
Igor Babiy <yegor.babiy@gmail.com>
2026-07-25 11:09:54 +0000

    add use auth hook

--- components/header.tsx
+++ components/header.tsx
@@ -1,27 +1,20 @@
 "use client";
 
-import { User as AuthUser } from "lucia";
 import { LucideKanban, LucideLogOut } from "lucide-react";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import { buttonVariants } from "@/components/ui/button";
-import { getAuth } from "@/features/auth/queries/get-auth";
+import { useAuth } from "@/features/auth/hooks/use-auth";
 import { signOut } from "@/features/ticket/actions/sign-out";
 import { homePath, signInPath, signUpPath, ticketsPath } from "@/paths";
 import { SubmitButton } from "./form/submit-button";
 import { ThemeSwitcher } from "./theme/theme-switcher";
 
 const Header = () => {
-  const [user, setUser] = useState<AuthUser | null>(null);
+  const { user, isFetched } = useAuth();
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      const { user } = await getAuth();
-      setUser(user);
-    };
-
-    fetchUser();
-  }, []);
+  if (!isFetched) {
+    return null;
+  }
 
   const navItems = user ? (
     <>
@@ -54,7 +47,7 @@ const Header = () => {
 
   return (
     <nav
-      className="supports-backdrop-blur:bg-background/60
+      className="animate-header-from-top supports-backdrop-blur:bg-background/60
             fixed left-0 right-0 top-0 z-20
             border-b bg-background/95 backdrop-blur
             w-full flex py-2.5 px-5 justify-between"
---
+++ features/auth/hooks/use-auth.ts
@@ -0,0 +1,22 @@
+import { User as AuthUser } from "lucia";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { getAuth } from "../queries/get-auth";
+
+export const useAuth = () => {
+  const [user, setUser] = useState<AuthUser | null>(null);
+  const [isFetched, setFetched] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { user } = await getAuth();
+      setUser(user);
+      setFetched(true);
+    };
+
+    fetchUser();
+  }, [pathname]);
+
+  return { user, isFetched };
+};
